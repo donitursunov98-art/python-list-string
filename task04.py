efdcfe06f5bed353a07d78d5c664dae1b35cd1cb
Abu <efootball042605@gmail.com>
2025-12-28 19:18:49 +0500

    4-mashq

--- task04.py
+++ task04.py
@@ -1,1 +1,17 @@
+text = input('input: ')
 
+def get_domains(text: str) -> list[str]:
+    emails = text.split(', ')
+
+    domains = []
+    for email in emails:
+        idx = email.find('@')
+        if idx != -1:
+            domain = email[idx:]
+            domains.append(domain)
+    
+    return domains        
+
+
+domains = get_domains(text=text)
+print(domains)
