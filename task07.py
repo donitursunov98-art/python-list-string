f1cee0cddf71be58d3bf5fe5f3e774de01faec98
Abu <efootball042605@gmail.com>
2025-12-28 19:10:24 +0500

    7-mashq

--- task07.py
+++ task07.py
@@ -1,1 +1,8 @@
+text = input('input: ')
 
+pairs = text.split(', ')
+
+for pair in pairs:
+    separated_pair = pair.split(':')
+    result = f'{separated_pair[0]}: {separated_pair[1]}'
+    print(result)
