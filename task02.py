9e5483dca600daaf52e3eec21947359cac83b6b5
Abu <efootball042605@gmail.com>
2025-12-28 19:25:56 +0500

    2-mashq

--- task02.py
+++ task02.py
@@ -1,1 +1,13 @@
+text = input('Input: ')
 
+items = text.split(', ')
+
+formatted_items = []
+
+for item in items:
+    item = item.lower()
+    item = item.replace(' ', '_')
+    formatted_items.append(item)
+result = '_'.join(formatted_items)
+
+print(result)
